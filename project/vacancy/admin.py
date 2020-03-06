a9636329e114bb099c18153b95388846abdcaa4e
KleiKoks <easyebengrad@gmail.com>
2020-03-06 15:18:58 +0200

    .

--- project/vacancy/admin.py
+++ project/vacancy/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class VacancyAdmin(admin.ModelAdmin):
+    exclude = ['']
+
+
+class GenderAdmin(admin.ModelAdmin):
+    exclude = ['']
+
+
+class DocumetTypeAdmin(admin.ModelAdmin):
+    exclude = ['']
+
+admin.site.register(Vacancy, VacancyAdmin)
+admin.site.register(Gender, GenderAdmin)
+admin.site.register(DocumetType, DocumetTypeAdmin)
