a71e0d3a4196f93901d3531326389a1556b1d17f
KleiKoks <easyebengrad@gmail.com>
2020-03-09 12:35:40 +0200

    .

--- project/blog/admin.py
+++ project/blog/admin.py
@@ -4,11 +4,8 @@ from .models import *
 class PostAdmin(admin.ModelAdmin):
     pass 
 
-class PostCategoryAdmin(admin.ModelAdmin):
-    pass 
 
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(PostCategory, PostCategoryAdmin)
 
 
