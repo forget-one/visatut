7fc36dfe508a2b77fef742d0505a0207735b2c64
andrew mendela <jurgeon018@gmail.com>
2020-06-03 08:20:48 +0000

    f

--- project/api/search.py
+++ project/api/search.py
@@ -10,20 +10,14 @@ def search_service(request):
     def vant5(value):
         if value == 'True': return True
         return False 
-    vacancies = Vacancy.objects.all()
-    country__title      = data.get('vant1')
-    gender__human_type  = data.get('vant2')
-    work_type__work_type= data.get('vant3')
-    document__doc_type  = data.get('vant4')
-    actual              = vant5(data.get('actual'))
-    if country__title: vacancies.filter(country__title=country__title)
-    if gender__human_type: vacancies.filter(gender__human_type=gender__human_type)
-    if work_type__work_type: vacancies.filter(work_type__work_type=work_type__work_type)
-    if document__doc_type: vacancies.filter(document__doc_type=document__doc_type)
-    if actual: vacancies.filter(actual=actual)
+    vacancies = Vacancy.objects.filter(
+        country__title      = data.get('vant1'),
+        gender__human_type  = data.get('vant2'),
+        work_type__work_type= data.get('vant3'),
+        document__doc_type  = data.get('vant4'),
+        actual              = vant5(data.get('actual'))
+    ).select_related('country', 'gender', 'document', 'work_type',)
     page, created = Page.objects.get_or_create(
         slug    = f"{request.build_absolute_uri()}")
-    return render(request, 'search_service.html', {
-        'vacancies': vacancies,
-    })
+    return render(request, 'search_service.html', locals())
     
