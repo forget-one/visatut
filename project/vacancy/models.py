915f1fcc88c461df5fbe23a4a433f592e2693c2a
kleikoks <kleikoks.py@agmail.ru>
2020-06-05 10:53:14 +0300

    search_db_url fix

--- project/vacancy/models.py
+++ project/vacancy/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from project.models import MetaData
+from tinymce import HTMLField
 # Create your models here.
 
 class Vacancy(models.Model):
@@ -8,12 +9,12 @@ class Vacancy(models.Model):
     gender          = models.ForeignKey(to='Gender', verbose_name='Робота для',  blank=True, null=True, on_delete=models.CASCADE)
     document        = models.ForeignKey(to='DocumetType', verbose_name='Необхідні документи', blank=True, on_delete=models.CASCADE, null=True, related_name='documents')
     work_type       = models.ForeignKey(to='WorkType', verbose_name='Тип роботи', on_delete=models.CASCADE, blank=True, null=True, related_name='work_types')
-    requirements    = models.TextField(verbose_name='Вимоги', blank=True, null=True)
+    requirements    = HTMLField(verbose_name='Вимоги', blank=True, null=True)
     work_position   = models.CharField(verbose_name='Адреса роботи', max_length=1000, blank=True, null=True)
     work_day        = models.CharField(verbose_name='Графік роботи', max_length=1000, blank=True, null=True)
-    your_duties     = models.TextField(verbose_name='Витрати працівників', blank=True, null=True)
-    our_duties      = models.TextField(verbose_name='Компанія надає', blank=True, null=True)
-    duties          = models.TextField(verbose_name="Обов'язки", blank=True, null=True)
+    your_duties     = HTMLField(verbose_name='Витрати працівників', blank=True, null=True)
+    our_duties      = HTMLField(verbose_name='Компанія надає', blank=True, null=True)
+    duties          = HTMLField(verbose_name="Обов'язки", blank=True, null=True)
     actual          = models.BooleanField(verbose_name="Актуальність", default=True, blank=True, null=True)
     def __str__(self):
         return f'{self.name}'
